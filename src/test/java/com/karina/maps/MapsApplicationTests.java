e573fb61dc008d0fbc18e2a7ef37a3b27a7cf81b
Karina Cardoso Telles <karinac.telles@gmail.com>
2020-08-25 11:17:39 -0300

    começo de teste dos testes

--- src/test/java/com/karina/maps/MapsApplicationTests.java
+++ src/test/java/com/karina/maps/MapsApplicationTests.java
@@ -1,13 +1,39 @@
 package com.karina.maps;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+/*Vem por padrão*/
 @SpringBootTest
 class MapsApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
+/**/
+/*Testa se uma afirmação (assertions) booleana é verdadeira */
+	@DisplayName("Escreve assertions para booleans")
+	class BooleanAssertionTest{
+	@Nested
+	@DisplayName("Se boolean é verdadeiro (true)")
+	class WhenBooleanIsTrue{
+		@Test
+		@DisplayName("Deve ser true")
+		void shouldBeTrue(){
+			assertThat(true).isTrue();
+		}
+	}
+	@Nested
+	@DisplayName("Se boolean é falso (false)")
+	class WhenBooleanIsFalse{
+		@Test
+		@DisplayName("Deve ser false")
+		void shouldBeFalse(){
+			assertThat(false).isFalse();
+		}
+	}
+	}
+	//@DisplayName("Writi")
 }
