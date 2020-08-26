9e3d3dfb2d85106158d532b7422b5ceae701ce46
Karina Cardoso Telles <karinac.telles@gmail.com>
2020-08-26 13:31:04 -0300

    testes

--- src/test/java/com/karina/maps/MapsApplicationTests.java
+++ src/test/java/com/karina/maps/MapsApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
-
+//https://www.petrikainulainen.net/programming/testing/junit-5-tutorial-writing-assertions-with-assertj/
 /*Vem por padrão*/
 @SpringBootTest
 class MapsApplicationTests {
@@ -35,5 +35,34 @@ class MapsApplicationTests {
 		}
 	}
 	}
-	//@DisplayName("Writi")
+	@DisplayName("Writing assertions for objects")
+	class ObjectAssertionTest {
+		@Nested
+		@DisplayName("When object is not null")
+		class WhenObjectIsNotNull{
+			@Test
+			@DisplayName("Should not be null")
+			void shouldNotBeNull(){
+				assertThat(new Object()).isNotNull();
+			}
+		}
+		@Nested
+		@DisplayName("When two objects are equal")
+		class WhenTwoObjectsAreEqual{
+			@Nested
+			@DisplayName("When objects are intengers")
+			class WhenObjectsAreIntengers{
+				private final Integer ACTUAL = 9;
+				private final Integer EXPECTED = 9;
+
+				@Test
+				@DisplayName("Should be equal")
+				void shouldBeEqual(){
+					assertThat(ACTUAL).isEqualByComparingTo(EXPECTED);
+				}
+			}
+		}
+	}
+
+
 }
