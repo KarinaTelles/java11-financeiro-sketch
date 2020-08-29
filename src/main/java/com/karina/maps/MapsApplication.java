4fef5c1453f0f814d50ea6afdb182ff6cd22f4a6
Karina Cardoso Telles <karinac.telles@gmail.com>
2020-08-29 16:14:07 -0300

    lógica com tratamento de erros

--- src/main/java/com/karina/maps/MapsApplication.java
+++ src/main/java/com/karina/maps/MapsApplication.java
@@ -10,35 +10,48 @@ public class MapsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MapsApplication.class, args);
+		/*Isso tudo é apenas para os lançamentos*/
+        try{
         System.out.println("*****************************************");
-		System.out.println("Entre com o valor do seu 1° lançamento: ");
+		System.out.println("(APENAS VALORES NUMÉRICOS) Entre com o valor do seu 1° lançamento: ");
 		Scanner inputValor = new Scanner(System.in);
 		int valor = Integer.parseInt(inputValor.nextLine());
-        if (valor < 1) {
-            System.out.println("Erro: valor inválida!!!");
-        }
-        else{
-            System.out.println("Seu lançamento tem o valor de R$" + valor + " (esse é o print 1)");
-            System.out.println("Entre com a descrição do seu 1° lançamento");
-            Scanner inputDescricao = new Scanner(System.in);
-            String descricao = inputDescricao.nextLine();
-            System.out.println(descricao);
-            System.out.println("*****************************************\n");
-            for (int i = 2; valor > 0; i++){
-                System.out.println("Entre com o valor do seu " + i + "° lançamento");
-                Scanner inputValorDois = new Scanner(System.in);
-                valor = Integer.parseInt(inputValorDois.nextLine());
-                if (valor < 1) {
-                    System.out.println("Erro: valor inválid0!!!");
+            if (valor == 0){
+                System.out.println("Poxa ein, vamos parar de gastar? seu saldo é 0, nada, vazio ):");
+            }
+            if (valor < 0) {
+                System.out.println("Erro: valor inválido!!!");
+            }else {
+                System.out.println("Seu lançamento tem o valor de R$" + valor + " (esse é o lançamento número: 1)");
+                System.out.println("Entre com a descrição do seu 1° lançamento");
+                Scanner inputDescricao = new Scanner(System.in);
+                String descricao = inputDescricao.nextLine();
+                System.out.println(descricao);
+                System.out.println("*****************************************\n");
+
+                try{
+                for (int i = 2; valor > 0; i++) {
+                    System.out.println("(APENAS VALORES NUMÉRICOS) Entre com o valor do seu " + i + "° lançamento");
+                    Scanner inputValorDois = new Scanner(System.in);
+                    valor = Integer.parseInt(inputValorDois.nextLine());
+                    if (valor == 0){
+                        System.out.println("Poxa ein, vamos parar de gastar? seu saldo é 0, nada, vazio ):");
+                    }
+                    if (valor < 0) {
+                        System.out.println("Erro: valor inválido!!!");
+                    } else {
+                        System.out.println("Seu lançamento tem o valor de R$" + valor + " (esse é o lançamento número: " + i + " )");
+                        System.out.println("Entre com a descrição do seu " + i + "° lançamento");
+                        descricao = inputDescricao.nextLine();
+                        System.out.println("*****************************************");
+                    }
                 }
-                else{
-                    System.out.println("Seu lançamento tem o valor de R$" + valor + " (esse é o print " + i + " )" );
-                    System.out.println("Entre com a descrição do seu "+i+ "° lançamento");
-                    descricao = inputDescricao.nextLine();
-                    System.out.println("*****************************************");
+            }catch (NumberFormatException e){
+                    System.out.println("Apenas lançamento númerico, nenhum caracter é permitido");
                 }
             }
+        }catch (NumberFormatException e){
+            System.out.println("Apenas lançamento númerico, nenhum caracter é permitido");
         }
-
 	}
 }
