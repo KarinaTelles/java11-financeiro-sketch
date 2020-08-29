a1b865b851838bcbd48e4f6561dbf0a76fa6202e
Karina Cardoso Telles <karinac.telles@gmail.com>
2020-08-29 14:52:58 -0300

    correção lógica interessante

--- src/main/java/com/karina/maps/MapsApplication.java
+++ src/main/java/com/karina/maps/MapsApplication.java
@@ -10,33 +10,35 @@ public class MapsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MapsApplication.class, args);
-
+        System.out.println("*****************************************");
 		System.out.println("Entre com o valor do seu 1° lançamento: ");
 		Scanner inputValor = new Scanner(System.in);
 		int valor = Integer.parseInt(inputValor.nextLine());
-		System.out.println("Seu lançamento tem o valor de R$" + valor + " (esse é o print 1) \n");
-
-//        System.out.println("Entre com a descrição do seu lançamento");
-//        Scanner inputDescricao = new Scanner(System.in);
-//        String descricao = inputDescricao.nextLine();
-
-//        while (valor > 0) {
-//            System.out.println("Entre com um valor");
-//            Scanner inputValorDois = new Scanner(System.in);
-//            valor = Integer.parseInt((inputValorDois.nextLine()));
-//        }
-
-        for (int i = 2; valor > 0; i++){
-
-            System.out.println("Entre com o valor do seu " + i + "° lançamento");
-            Scanner inputValorDois = new Scanner(System.in);
-            valor = Integer.parseInt(inputValorDois.nextLine());
-            System.out.println("Seu lançamento tem o valor de R$" + valor + " (esse é o print " + i + " ) \n" );
-
-        }
-
         if (valor < 1) {
-            System.out.println("Erro: Entrada inválida!!!");
+            System.out.println("Erro: valor inválida!!!");
         }
+        else{
+            System.out.println("Seu lançamento tem o valor de R$" + valor + " (esse é o print 1)");
+            System.out.println("Entre com a descrição do seu 1° lançamento");
+            Scanner inputDescricao = new Scanner(System.in);
+            String descricao = inputDescricao.nextLine();
+            System.out.println(descricao);
+            System.out.println("*****************************************\n");
+            for (int i = 2; valor > 0; i++){
+                System.out.println("Entre com o valor do seu " + i + "° lançamento");
+                Scanner inputValorDois = new Scanner(System.in);
+                valor = Integer.parseInt(inputValorDois.nextLine());
+                if (valor < 1) {
+                    System.out.println("Erro: valor inválid0!!!");
+                }
+                else{
+                    System.out.println("Seu lançamento tem o valor de R$" + valor + " (esse é o print " + i + " )" );
+                    System.out.println("Entre com a descrição do seu "+i+ "° lançamento");
+                    descricao = inputDescricao.nextLine();
+                    System.out.println("*****************************************");
+                }
+            }
+        }
+
 	}
 }
--- src/main/java/com/karina/maps/com/karina/maps/conta/corrente/Saldos.java
+++ src/main/java/com/karina/maps/com/karina/maps/conta/corrente/Saldos.java
@@ -8,32 +8,6 @@ public class Saldos {
 
     boolean teste(){
 
-//        System.out.println("Entre com o valor do seu lançamento");
-//        Scanner inputValor = new Scanner(System.in);
-//        int valor = Integer.parseInt(inputValor.nextLine());
-//        System.out.println("Seu lançamento tem o valor de" + valor + "print 1");
-
-//        System.out.println("Entre com a descrição do seu lançamento");
-//        Scanner inputDescricao = new Scanner(System.in);
-//        String descricao = inputDescricao.nextLine();
-
-//        while (valor > 0) {
-//            System.out.println("Entre com um valor");
-//            Scanner inputValorDois = new Scanner(System.in);
-//            valor = Integer.parseInt((inputValorDois.nextLine()));
-//        }
-
-//        for(int i = 0; valor > 1; i++){
-//            System.out.println("Entre com um valor");
-//            Scanner inputValorDois = new Scanner(System.in);
-//            valor = Integer.parseInt(inputValorDois.nextLine());
-//            System.out.println("Seu lançamento tem o valor de" + valor "print 2");
-//        }
-
-//        if (valor < 1) {
-//            System.out.println("Entrada inválida!!!");
-//        }
-
         return true;
     }
 
