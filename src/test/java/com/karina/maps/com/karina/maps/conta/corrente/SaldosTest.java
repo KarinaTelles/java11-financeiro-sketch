1542f6b0bbb72725618437261c242fa57d5ce0b7
Karina Cardoso Telles <karinac.telles@gmail.com>
2020-08-31 11:42:51 -0300

    ufa

--- src/test/java/com/karina/maps/com/karina/maps/conta/corrente/SaldosTest.java
+++ src/test/java/com/karina/maps/com/karina/maps/conta/corrente/SaldosTest.java
@@ -11,20 +11,10 @@ class SaldosTest {
 
     //Testa se o lançamento não é negativo
     @Test
-    void lancamentoEntrada() {
-        Saldos saldoEntrada = new Saldos();//arrange
-        int responseEntrada = saldoEntrada.lancamentoEntrada();//act
-        assertThat(responseEntrada).isNotNegative();//assert
-        AssertionsForClassTypes.assertThat(responseEntrada).isNotNegative();
-        AssertionsForClassTypes.assertThat(responseEntrada).isNotZero();
-    }
-
-    @Test
-    void lancamentoSaida() {
-        Saldos saldoSaida = new Saldos();
-        int responseSaida = saldoSaida.getGuardaValor();
-        assertThat(responseSaida).isNotNegative();
-        assertThat(responseSaida).isNotZero();
+    void lancamento() {
+        Saldos saldo = new Saldos();
+        int response = saldo.getGuardaValor();
+        assertThat(response).isNotNegative();
     }
 
     @Test
