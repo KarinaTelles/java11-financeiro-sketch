3e69f51ef7dda5933dd092290141916434325776
Karina Cardoso Telles <karinac.telles@gmail.com>
2020-08-30 21:10:39 -0300

    testes testes testes

--- src/test/java/com/karina/maps/com/karina/maps/conta/corrente/SaldosTest.java
+++ src/test/java/com/karina/maps/com/karina/maps/conta/corrente/SaldosTest.java
@@ -3,14 +3,19 @@ package com.karina.maps.com.karina.maps.conta.corrente;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("Testa o fluxo de dados do saldo")
 class SaldosTest {
 
+    //Testa se o lançamento não é negativo
     @Test
     void lancamentoEntrada() {
-        Saldos saldos = new Saldos();
+        Saldos saldos = new Saldos();//arrange
+        int response = saldos.lancamentoEntrada();//act
+        assertThat(response).isNotNegative();
     }
+
 
     @Test
     void lancamentoSaida() {
