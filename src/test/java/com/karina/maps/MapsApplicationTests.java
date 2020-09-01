930341d9ea1f156e5855ab136ad276f79555dc83
Karina Cardoso Telles <karinac.telles@gmail.com>
2020-09-01 06:45:16 -0300

    ajuste de import

--- src/test/java/com/karina/maps/MapsApplicationTests.java
+++ src/test/java/com/karina/maps/MapsApplicationTests.java
@@ -1,10 +1,7 @@
 package com.karina.maps;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class MapsApplicationTests {
 	@Test
